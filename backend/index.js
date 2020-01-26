7d63bfd07fefe3a22e11f526028cda5d0216aeab
Justin Cotarla <justin.cotarla@gmail.com>
2020-01-26 03:52:45 -0500

    add POST /split

--- backend/index.js
+++ backend/index.js
@@ -3,76 +3,68 @@ const axios = require('axios');
 const sharp = require('sharp');
 const {search,getSong} = require('./catalog.queries')
 const express = require('express');
-const shutterstock = require("shutterstock-api");
 const cors = require('cors');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
+// SETUP EXPRESS
 const app = express();
-app.use(cors())
+app.use(bodyParser.json());
+app.use(cors());
+app.use(morgan('tiny'));
 
 const PORT = 8080;
 
+// SETUP SHUTTERSTOCK API
 const SHUTTERSTOCK_API_TOKEN = 'QXJKWVIwUFNybFNGWU53bEVDdHVvYjVRd1ROWURyTEc6QVFRYVRvMWxKcjFHaXM4aw==';
 const shutterstockApi = axios.create({ 
     baseURL: 'https://api.shutterstock.com',
     headers: { 'Authorization': `Basic ${SHUTTERSTOCK_API_TOKEN}`},
 });
 
-shutterstock.setBasicAuth(SHUTTERSTOCK_API_TOKEN);
-
-const imagesApi = new shutterstock.ImagesApi();
-
 const COMPRESSED_IMAGE_RES = 16;
 
-const TEST_IMAGE_URL = "http://st.cdjapan.co.jp/pictures/s/13/48/NEOIMP-13200.jpg?v=6";
-
-const stitch = async () => {
-    // Download image
-    const response = await axios({
-        method: "GET",
-        url: TEST_IMAGE_URL,
-        responseType: "arraybuffer",
-    });
-
+/**
+ * Compresses the source image
+ * @return stock images corresponding to each pixel in the compressed image
+ */
+const split = async (inputImageBuffer) => {
     // Compress image
-    const imageBuffer = await sharp(response.data)
+    const imageBuffer = await sharp(inputImageBuffer)
         .resize(COMPRESSED_IMAGE_RES, COMPRESSED_IMAGE_RES)
         .raw()
         .toBuffer();
 
-
-    hexColors = [];
-
+    // Convert to hex array
+    let hexColors = [];
     for (i = 0; i < imageBuffer.length; i+=3) {
         hexColors.push(`${imageBuffer[i].toString(16)}${imageBuffer[i+1].toString(16)}${imageBuffer[i+2].toString(16)}`)
     }
 
-    console.log(hexColors);
-
-    // For each pixel, search ShutterStock for images with color
-    try {
-        const response = await shutterstockApi.get('/v2/images/search', {
-            params: {
-                color: '00ff00',
-                height_from: '128',
-                height_to: '128',
-                width_from: '128',
-                width_to: '128',
-            },
-        });
-
-        const firstResult = response.data.data[0];
-        console.log(firstResult)
-    } catch (error) {
-        console.error(error);        
-    }
-
-    // Normalize 
+    const requests = hexColors.map(hexColor => shutterstockApi.get('/v2/images/search', {
+        params: {
+            color: hexColor,
+            per_page: 1,
+        },
+    }));
+    
+    const results = await Promise.all(requests);
+    const data = results.map(({ data }) => data.data[0]);
+    
+    return data;
 }
 
-app.listen(PORT, () => console.log(`Ready to stitch on port ${PORT}`));
+app.listen(PORT, () => console.log(`Ready to split on port ${PORT}`));
 
-app.post('/stitch', () => {
-    stitch();
+app.post('/split', async (req, res) => {
+    const { imageUrl } = req.body;
+    const response = await axios({
+        method: 'GET',
+        url: imageUrl,
+        responseType: 'arraybuffer',
+    });
+    const results = await split(response.data);
+    return res.json(results);
 });
 
 app.get("/songs/:query", async (req,res)=> {
@@ -119,6 +111,3 @@ app.get("/song/:songId", async (req,res)=> {
                         .catch(e => res.status(500).send({error: e}))
     res.json({song})
 });
-
-
-stitch();
